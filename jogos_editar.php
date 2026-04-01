b0bd492ee6c5f44dad77e00e6e78cbeb02a19a90
Rafael Jaques <rafael@phpit.com.br>
2026-04-01 11:31:40 -0300

    Formulário de edição

---
+++ jogos_editar.php
@@ -0,0 +1,33 @@
+<?php
+// jogos_excluir.php
+require('carregar_pdo.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id = (int) $_POST['id'] ?? false;
+    
+    die;
+}
+
+
+$id = (int) $_GET['id'] ?? false;
+
+if (!$id) {
+    header('location:jogos.php');
+    die;
+}
+
+require('carregar_twig.php');
+
+$dados = $pdo->prepare('SELECT * FROM jogos WHERE id = :id');
+$dados->execute([':id' => $id]);
+
+if ($dados->rowCount() != 1) {
+    header('location:jogos.php');
+    die;
+}
+
+$jogo = $dados->fetch(PDO::FETCH_ASSOC);
+
+echo $twig->render('jogos_editar.html', [
+    'jogo' => $jogo,
+]);
