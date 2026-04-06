2bac2938d445051edb52d8514866a9ab636c9471
Rafael Jaques <rafael@phpit.com.br>
2026-04-06 17:21:45 -0300

    Atualização de imagem de capa

--- jogos_editar.php
+++ jogos_editar.php
@@ -1,10 +1,42 @@
 <?php
-// jogos_excluir.php
+// jogos_editar.php
 require('carregar_pdo.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $id = (int) $_POST['id'] ?? false;
+    $nome = $_POST['nome'] ?? false;
+    $estilo = $_POST['estilo'] ?? false;
+
+    // Verifica se há nova capa
+    if (!$_FILES['capa']['error']) {
+        // Descobre nome do arquivo anterior
+        $dados = $pdo->prepare('SELECT capa FROM jogos WHERE id = :id');
+        $dados->execute([':id' => $id]);
+        $capa_velha = $dados->fetch(PDO::FETCH_ASSOC)['capa'];
+        // Apagar a capa
+        $capa_velha = __DIR__ . '/img/' . $capa_velha;
+        
+        if (file_exists($capa_velha)) {
+            unlink($capa_velha);
+        }
+        // Gravar a nova capa
+        $ext = pathinfo($_FILES['capa']['name'], PATHINFO_EXTENSION);
+        $capa = uniqid().'.'.$ext;
+        move_uploaded_file($_FILES['capa']['tmp_name'], "img/{$capa}");
+    }
+
+    $sql = 'UPDATE jogos SET nome = :nome, estilo = :estilo'.(isset($capa) ? ', capa = :capa' : '').' WHERE id = :id';
     
+    $dados = $pdo->prepare($sql);
+    $params = [
+        ':id' => $id,
+        ':nome' => $nome,
+        ':estilo' => $estilo,
+    ];
+    if (isset($capa)) { $params[':capa'] = $capa; }
+    $dados->execute($params);
+
+    header('location: jogos.php');
     die;
 }
 
