05826a0ce4212619929c0f1d69967f85c13439e2
Rafael Jaques <rafael@phpit.com.br>
2026-04-24 10:37:40 -0300

    Carbon

--- jogos.php
+++ jogos.php
@@ -3,6 +3,8 @@
 require('carregar_twig.php');
 require('carregar_pdo.php');
 
+use Carbon\Carbon;
+
 $jogos = $pdo->query('SELECT * FROM jogos');
 $todosJogos = $jogos->fetchAll(PDO::FETCH_ASSOC);
 
--- jogos_inserir.php
+++ jogos_inserir.php
@@ -6,9 +6,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Preparar os dados para inserir no banco
     $nome = $_POST['nome'] ?? false;
     $estilo = $_POST['estilo'] ?? false;
+    $lancamento = $_POST['lancamento'] ?? false;
 
     // Verifica erro
-    if (!$nome || !$estilo) {
+    if (!$nome || !$estilo || !$lancamento) {
         $erro = 'Preencha todos os campos';
     } else {
         // Tudo certo - gravar os dados
@@ -18,12 +19,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         move_uploaded_file($_FILES['capa']['tmp_name'], "img/{$capa}");
 
         require('carregar_pdo.php');
-        $dados = $pdo->prepare('INSERT INTO jogos (nome, estilo, capa) VALUES (?, ?, ?)');
+        $dados = $pdo->prepare('INSERT INTO jogos (nome, estilo, capa, lancamento) VALUES (?, ?, ?, ?)');
         
         $dados->bindParam(1, $nome);
         $dados->bindParam(2, $estilo);
         $dados->bindParam(3, $capa);
-
+        $dados->bindParam(4, $lancamento);
         $dados->execute();
 
         header('location:jogos.php');
